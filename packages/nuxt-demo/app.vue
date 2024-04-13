ce490a2f47fdc78c9a202237460929048b9df7ae
hojas <cyxhuan9@163.com>
2024-04-13 12:17:13 +0800

    feat(nuxt-demo): add nuxt-demo

---
+++ packages/nuxt-demo/app.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { VueMermaidRender } from 'vue-mermaid-render'
+
+const content = ref(`
+graph TD;
+  A-->B;
+  A-->C;
+  B-->D;
+  C-->D;
+`)
+
+setTimeout(() => {
+  content.value = `
+  sequenceDiagram
+    participant Alice
+    participant Bob
+    Alice->>John: Hello John, how are you?
+    loop Healthcheck
+        John->>John: Fight against hypochondria
+    end
+    Note right of John: Rational thoughts <br/>prevail!
+    John-->>Alice: Great!
+    John->>Bob: How about you?
+    Bob-->>John: Jolly good!
+`
+}, 2000)
+</script>
+
+<template>
+  <VueMermaidRender :content="content" />
+</template>
--- scripts/dev.cjs
+++ scripts/dev.cjs
@@ -1,7 +1,45 @@
+const process = require('node:process')
 const path = require('node:path')
 const shell = require('shelljs')
 
 const demo = path.resolve(__dirname, '../packages/demo/')
+const nuxtDemo = path.resolve(__dirname, '../packages/nuxt-demo/')
 
-shell.cd(demo)
-shell.exec('pnpm run dev')
+function runDemo() {
+  shell.cd(demo)
+  shell.exec('pnpm run dev')
+}
+
+function runNuxtDemo() {
+  shell.cd(nuxtDemo)
+  shell.exec('pnpm run dev')
+}
+
+let demoType = ''
+try {
+  demoType = process.argv[2].split('=')[1]
+}
+catch {}
+
+const demoMap = {
+  vue: runDemo,
+  nuxt: runNuxtDemo,
+}
+
+if (!demoType) {
+  console.log('demo type is required')
+  console.log('demo type: vue or nuxt')
+  console.log('example: pnpm dev --demo=vue')
+  process.exit(1)
+}
+
+if (!demoMap[demoType]) {
+  console.error('demo type error')
+  console.log('demo type: vue or nuxt')
+  console.log('example: pnpm dev --demo=vue')
+  process.exit(1)
+}
+
+console.log(`run ${demoType} demo`)
+
+demoMap[demoType]()
